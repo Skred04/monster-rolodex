8c4c75ccc2c219842d3635e3f7fc807b2673681f
Pier Philippon <pier.philippon@gmail.com>
2019-10-15 19:36:54 +0200

    Updates

---
+++ precache-manifest.010d2e500eddcd4cfee02586c6f318db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1475f19481c2e4cf062da4fc993fa009",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "5363cb95f02b48b834b7",
+    "url": "/monster-rolodex/static/css/main.899fdf65.chunk.css"
+  },
+  {
+    "revision": "56022eb4a5dc281792e1",
+    "url": "/monster-rolodex/static/js/2.66e5ab65.chunk.js"
+  },
+  {
+    "revision": "5363cb95f02b48b834b7",
+    "url": "/monster-rolodex/static/js/main.b9d1d275.chunk.js"
+  },
+  {
+    "revision": "6f70f97b8cfdb2d06998",
+    "url": "/monster-rolodex/static/js/runtime-main.aec4d7d7.js"
+  }
+]);
